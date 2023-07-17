cc844d920c7ca0f0b2ddf5fa90ccc0299fa3e840
Jenny <JenifferLeutert@gmx.de>
2023-07-17 10:14:11 +0200

    Funktionsaufruf

--- script_01.js
+++ script_01.js
@@ -3,3 +3,14 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+//Funktionsrumpf (body) | callee
+// Funktionsgeklaration
+function test()
+{
+console.log("Hallo Max!");
+}
+
+// Funktionsaufruf (call)
+
+test();
+
