1c98bb2b05a5d5db7068865d8019515ac50065d9
Jenny <JenifferLeutert@gmx.de>
2023-07-17 12:26:32 +0200

    wrapper funktion

--- script_01.js
+++ script_01.js
@@ -45,10 +45,40 @@ function ausgabeNamen2(firstName)
     console.log("Hallo " + firstName + "!");
 }
 
-ausgabeNamenParams("Max", "Mütze");
-ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+//ausgabeNamenParams("Max", "Mütze");
+//ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
 
 function ausgabeNamenParams(firstName, familyName) 
 {
     console.log("Hallo " + firstName + " " + familyName + "!"); 
 }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenParams2("Max", "Mütze");
+
+function ausgabeNamenParams2(firstName, familyName) 
+{// 1. Funktionalität: string composing
+ 
+const GAP = " ";
+let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    //2 Funktionalität: string output
+    console.log(outputStr); 
+}
+
+/***** Funktionen 03b *****/
+// 03b Funktionalitäten --> Funktionen| return
+
+// 2. Funktionalität: string output
+//output("Hi");
+//output (3);
+//output(true);
+
+function output(outputData) 
+{
+console.log(outputData);    
+}
+
