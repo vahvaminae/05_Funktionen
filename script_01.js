faf9396b8487ac3b627303662cc97253fe29ac4a
Jenny <JenifferLeutert@gmx.de>
2023-07-17 10:39:58 +0200

    Scope Fehler erzeugen

--- script_01.js
+++ script_01.js
@@ -14,3 +14,16 @@ console.log("Hallo Max!");
 
 test();
 
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+ausgabeNamen();
+
+function ausgabeNamen() 
+{
+   // console.log("Hallo Max!");
+    //console.log("Hallo Lucas!");
+    let firstName = "Lucas";
+    console.log("Hallo " + firstName + "!");
+}
+
+//console.log(firstName); //Fehler --> SCOPE! (gültigkeitsbereich von Variablen)
